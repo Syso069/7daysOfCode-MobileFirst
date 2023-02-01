741a9af1bd3e01dd821272a61e7f77a541c610db
Siso Matheus <sisomd69@gmail.com>
2023-02-01 18:45:30 -0300

    add funcionalidades para o botao (ponto flutuante)

--- index.js
+++
@@ -1,6 +0,0 @@
-const botaoMenu = document.querySelector('.imagem__ponto-flutuante')
-const menu = document.querySelector('.menuLateral')
-
-botaoMenu.addEventListener('click',() => {
-    menu.classList.toggle('menu__lateral-ativo')
-})
